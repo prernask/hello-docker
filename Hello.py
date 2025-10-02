89cc2e9363ad971e58c7794767029a35e57ac680
Prerana Karande <91388114+prernask@users.noreply.github.com>
2025-10-02 11:29:41 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,4 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return "Hello from Dockerized Flask App on EC2!"
 
-print("hello world");
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
